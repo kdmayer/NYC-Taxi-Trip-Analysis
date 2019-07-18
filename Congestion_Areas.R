94e69dd483945ae48804cca73d0ba0b66a932de5
Kevin <49768291+kdmayer@users.noreply.github.com>
2019-07-18 19:41:22 +0200

    Add files via upload

---
+++ Congestion_Areas.R
@@ -0,0 +1,79 @@
+rm(list=ls())
+
+library(Rtsne) 
+library(ggplot2) 
+library(cluster)
+library(Metrics)
+library(tibble)
+library(lubridate)
+library(stats)
+library(dplyr)
+library(kernlab)
+library(caret)
+library(e1071)
+library(neuralnet)
+
+taxi_trips <- read.csv("Taxi_120117.csv")
+
+taxi_trips <- taxi_trips[sample(nrow(taxi_trips), 1000), ]
+
+taxi_trips$tpep_dropoff_datetime <- as.character(taxi_trips$tpep_dropoff_datetime)
+taxi_trips$tpep_pickup_datetime <- as.character(taxi_trips$tpep_pickup_datetime)
+
+taxi_trips$tpep_dropoff_datetime <- as.POSIXct(taxi_trips$tpep_dropoff_datetime, format='%Y-%m-%d %H:%M:%S')
+taxi_trips$tpep_pickup_datetime  <- as.POSIXct(taxi_trips$tpep_pickup_datetime, format='%Y-%m-%d %H:%M:%S')
+
+# Because IDs are not in a quantitative relation, we convert their values into factors
+taxi_trips$PULocationID <- as.factor(taxi_trips$PULocationID)
+taxi_trips$DOLocationID <- as.factor(taxi_trips$DOLocationID)
+
+#"tpep_pickup_datetime","tpep_dropoff_datetime",
+
+taxi_trips <- select(taxi_trips,"mph","PULocationID","DOLocationID")
+
+#boxplot(taxi_trips$mph)
+
+#cut out outliers w.r.t. mph
+taxi_trips <- subset(taxi_trips, mph < 30 & mph >= 0)
+
+gower_dist <- daisy(taxi_trips,metric = "gower",type = list(logratio = 3))
+
+#output most similar trip
+gower_mat <- as.matrix(gower_dist)
+
+taxi_trips[which(gower_mat == min(gower_mat[gower_mat != min(gower_mat)]),
+        arr.ind = TRUE)[1, ], ]
+
+#output most dissimilar trip
+taxi_trips[which(gower_mat == max(gower_mat[gower_mat != max(gower_mat)]),
+        arr.ind = TRUE)[1, ], ]
+
+# Calculate silhouette width for many k using PAM
+
+#sil_width <- c(NA)
+
+#for(i in 2:10){
+  
+#  pam_fit <- pam(gower_dist,diss = TRUE,k = i)
+  
+#  sil_width[i] <- pam_fit$silinfo$avg.width
+  
+#}
+
+# Plot sihouette width (higher is better)
+
+#plot(1:10, sil_width,xlab = "Number of clusters",ylab = "Silhouette Width")
+#lines(1:10, sil_width)
+
+#assuming we take k = 5 as a reasonable amount of clusters
+pam_fit <- pam(gower_dist, diss = TRUE, k = 5)
+
+pam_results <- taxi_trips %>%
+  mutate(cluster = pam_fit$clustering) %>%
+  group_by(cluster) %>%
+  do(the_summary = summary(.))
+
+pam_results$the_summary
+
+
+
---
+++ Travel_Time_Predictor.R
@@ -0,0 +1,67 @@
+rm(list=ls())
+
+library(Metrics)
+library(tibble)
+library(lubridate)
+library(stats)
+library(dplyr)
+library(kernlab)
+library(caret)
+library(e1071)
+library(neuralnet)
+
+taxi_trips <- read.csv("Taxi_120117.csv")
+
+taxi_trips <- taxi_trips[sample(nrow(taxi_trips), 10000), ]
+
+taxi_trips$tpep_dropoff_datetime <- as.character(taxi_trips$tpep_dropoff_datetime)
+taxi_trips$tpep_pickup_datetime <- as.character(taxi_trips$tpep_pickup_datetime)
+
+taxi_trips$tpep_dropoff_datetime <- as.POSIXct(taxi_trips$tpep_dropoff_datetime, format='%Y-%m-%d %H:%M:%S')
+taxi_trips$tpep_pickup_datetime  <- as.POSIXct(taxi_trips$tpep_pickup_datetime, format='%Y-%m-%d %H:%M:%S')
+
+# Because IDs are not in a quantitative relation, we convert their values into factors
+taxi_trips$PULocationID <- as.factor(taxi_trips$PULocationID)
+taxi_trips$DOLocationID <- as.factor(taxi_trips$DOLocationID)
+
+sum_taxi_trips <- select(taxi_trips,"trip_duration","passenger_count","trip_distance","payment_type","total_amount")
+
+# provides a summary statistic for specific features
+summary(sum_taxi_trips)
+
+
+# predict estimated travel time based on pick up ID, pick up date time, drop off ID, and trip distance
+
+taxi_model1_SVM <- select(taxi_trips, "tpep_pickup_datetime", "trip_distance","PULocationID","DOLocationID", "trip_duration")
+
+# create training and testing data sets (70% of data for training)
+
+train_sample <- sample(10000,7000)
+summary(taxi_model1_SVM)
+
+#boxplot(taxi_model1_SVM$trip_duration)
+
+
+travel_time_train <- taxi_model1_SVM[train_sample,]
+travel_time_train <- subset(travel_time_train, trip_duration < 5000 & trip_duration > 0)
+travel_time_test <- taxi_model1_SVM[-train_sample,]
+travel_time_test <- subset(travel_time_test, trip_duration < 5000 & trip_duration > 0)
+
+# build SVM wit linear kernel
+
+travel_time_model <- ksvm(trip_duration ~ ., data = travel_time_train, 
+                          kernel = "rbfdot")
+
+travel_time_pred <- predict(travel_time_model,travel_time_test)
+
+summary(travel_time_pred)
+
+summary(travel_time_test[,5])
+
+# evaluate results based on MSE
+
+plot(travel_time_test$trip_duration, travel_time_pred)
+
+MAE(travel_time_pred, travel_time_test$trip_duration)
+
+cor(travel_time_pred, travel_time_test$trip_duration)
